21229ef222167b682b85a03c2a9fb6d1c5a9735c
Aditya Kumar Puri <113080776+puri-adityakumar@users.noreply.github.com>
2023-10-16 22:14:13 +0530

    Create arrInsertBegg.c

---
+++ C-Programming/Array/arrInsertBegg.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[50],i,n,x;
+    
+    printf("Enter the size of elements: ");
+    scanf("%d",&n); // "n" is size of the array
+    if(n >= 50)
+        printf("Overflow Condition");
+    else{
+        printf("Enter the elements for the array:");
+        for(i=0; i<n; i++){
+            scanf("%d", &arr[i]);
+        }
+        printf("Elements in the array are as follow:\n");
+     for(i=0; i<n; i++){
+         printf(" %d ",arr[i]);
+     }
+     
+     //Add a element inside the array at the beginning
+     
+     printf("\nEnter the element you want to add: ");
+     scanf("%d", &x); // "x" is the element that is going to inserted
+     
+     for(i=n; i>0; i--){
+         arr[i] = arr[i -1];
+     }
+     arr[0] = x;
+     
+     printf("Updated array: \n");
+     for(i=0;i<n+1;i++){
+         printf(" %d ",arr[i]);
+     }
+     
+     
+    }
+}
