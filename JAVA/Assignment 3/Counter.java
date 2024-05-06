4950d33d22f82265fa32d52b3e117556a5fbb272
Aditya Kumar Puri <113080776+puri-adityakumar@users.noreply.github.com>
2024-05-06 11:37:57 +0530

    Assignment 3 : D

--- JAVA/Assignment 3/Counter.java
+++ JAVA/Assignment 3/Counter.java
@@ -1,31 +1,31 @@
-
-/* 
-  Create a class called Counter with a static data member count and a static method incrementCounter. 
-  Implement a parameterized constructor that increments the count every time an object is created. Create objects of the class and display the final count.
+/*  Create a class called Counter with a static data member count and a static method incrementCounter. 
+    Implement a parameterized constructor that increments the count every time an object is created. 
+    Create objects of the class and display the final count.
 */
+import java.util.*;
 
+public class Counter {
 
-class Counter {
-  private static int  count = 0;
-  static void incrementCounter() {
-    count++;
-  }
-  
-  Counter() {
-    incrementCounter();
-  }
-  
-  static int getCount() {
-    return count;
-  } 
-  
-  public static void main(String s[]) {
+    static int count = 0;
+
+    Counter() {
+        count++;
+    }
+    public static int getCount() {
+        return count;
+    }
+
+    public static void incrementCounter() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+        System.out.println("The final count is " + Counter.getCount());
+    }
+
     
-    Counter obj1 = new Counter();
-    Counter obj2 = new Counter();
-    Counter obj3 = new Counter();
-    
-    System.out.println("Final Count: " + getCount());
-  }
 }
-class Counter ()
